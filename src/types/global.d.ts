0acb88daaf31c88b6c1c7565db03c9e21ff0bfdc
Arpan Parekh <arpanparekh06@gmail.com>
2025-06-17 13:59:12 +0530

    updated the project

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,10 +1,10 @@
-declare global {
-    namespace React {
-      interface ReactNode {
-        // Override to be more permissive
-      }
-    }
-  }
+// declare global {
+//     namespace React {
+//       interface ReactNode {
+//         // Override to be more permissive
+//       }
+//     }
+//   }
   
   export {};
   
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,3 @@
 /// <reference types="vite/client" />
 
-declare module 'react' {
-    interface ReactNode {
-      // Override to allow any type
-    }
-  }
   
